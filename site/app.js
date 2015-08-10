06c8a49ec68a212528fd69307a27c6714c17f374
wang zuo <wzuoadjusted@gmail.com>
2015-08-11 00:54:41 +0800

    simple <App/>

--- site/app.js
+++ site/app.js
@@ -1,6 +1,61 @@
 var React = require('react');
 var Header = require('./header');
 var Footer = require('./footer');
+var Nav = require('../lib/nav');
+
+var sections = require('./pages');
+
+var pages = {
+  'getting-started': {},
+  'react': {
+    'feng-form': require('./react/feng-form')
+  },
+  css: {
+    buttons: require('./css/buttons'),
+    forms: require('./css/forms'),
+    tabs: require('./css/tabs'),
+    card: require('./css/card'),
+    grid: require('./css/grid'),
+    labels: require('./css/labels'),
+    typography: require('./css/typography')
+  }
+}
+
+
+var Panel = React.createClass({
+  displayName: 'Panel',
+
+  renderPage(section, page) {
+    // console.log('renderPage', section, page);
+    var component = pages[section][page];
+    if(!component) throw new Error(`${section}/${page} not found`);
+    return React.createElement(component);
+  },
+
+  render() {
+    var path = this.props.path;
+    var ps = path.split('/');
+    var section = ps[1];
+    var page = ps[2];
+    var current = sections.filter((page) => page.path === section)[0];
+
+    return (
+      <div className="g-container">
+        <div className="g-r">
+          <div className="g-3">
+            <Nav
+              className="u-nav u-nav-y"
+              items={current.pages}
+            />
+          </div>
+          <div className="g-9">
+            {this.renderPage(section, page)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
 module.exports = React.createClass({
   displayName: 'App',
@@ -9,7 +64,7 @@ module.exports = React.createClass({
     return (
       <div className="app">
         <Header/>
-        {this.props.children}
+        <Panel path={this.props.path}/>
         <Footer/>
      </div>
     );
--- site/index.js
+++ site/index.js
@@ -21,65 +21,12 @@ if(__DEV__) {
 var history2 = require('history2');
 var React = require('react');
 // todo may use haml or sth else
-var sections = require('./pages');
 var App = require('./app');
-var Nav = require('../lib/nav');
-
-var pages = {
-  'getting-started': {},
-  'react': {
-    'feng-form': require('./react/feng-form')
-  },
-  css: {
-    buttons: require('./css/buttons'),
-    forms: require('./css/forms'),
-    tabs: require('./css/tabs'),
-    card: require('./css/card'),
-    grid: require('./css/grid'),
-    labels: require('./css/labels'),
-    typography: require('./css/typography')
-  }
-}
-
-var Panel = React.createClass({
-  displayName: 'Panel',
-
-  renderPage(section, page) {
-    // console.log('renderPage', section, page);
-    var component = pages[section][page];
-    if(!component) throw new Error(`${section}/${page} not found`);
-    return React.createElement(component);
-  },
-
-  render() {
-    var path = this.props.path;
-    var ps = path.split('/');
-    var section = ps[1];
-    var page = ps[2];
-    var current = sections.filter((page) => page.path === section)[0];
-
-    return (
-      <div className="g-container">
-        <div className="g-r">
-          <div className="g-3">
-            <Nav
-              className="u-nav u-nav-y"
-              items={current.pages}
-            />
-          </div>
-          <div className="g-9">
-            {this.renderPage(section, page)}
-          </div>
-        </div>
-      </div>
-    );
-  }
-});
 
 if(__DEV__) {
   function route(path) {
     React.render(
-      <App path={path}><Panel path={path}/></App>,
+      <App path={path}></App>,
       document.getElementById('app')
     );
   }
@@ -106,5 +53,5 @@ if(__DEV__) {
 }
 
 module.exports = function(path) {
-  return React.renderToString(<App path={path}><Panel path={path}/></App>);
+  return React.renderToString(<App path={path}/>);
 };
