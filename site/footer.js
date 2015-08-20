341296888eedd2450aa842a2e05db27fd412695c
wang zuo <wzuoadjusted@gmail.com>
2015-08-20 21:53:03 +0800

    footer updates

--- site/footer.js
+++ site/footer.js
@@ -1,12 +1,28 @@
 var React = require('react');
+var Grid = require('../lib/grid');
 
 module.exports = React.createClass({
   displayName: 'Footer',
 
   render() {
+    var year = new Date().getFullYear();
+
     return (
       <div className="m-footer">
-        feng-ui
+        <Grid.Container>
+          <Grid.Row>
+            <Grid.Column d={3}>
+              <nav>
+                <a href="https://github.com/wangzuo/feng-ui">Github</a>
+                <a href="https://npmjs.com/package/feng-ui">NPM</a>
+                <a href="#">About</a>
+              </nav>
+              <div className="leadtext">
+                Copyright &copy; 2015 (ISC) License
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid.Container>
       </div>
     );
   }
