49e4bfa84c747bc287abdd0e921f564f3e178afe
Wang Zuo <wzuoadjusted@gmail.com>
2015-09-18 15:25:17 +0800

    add json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.json$/, loader: 'json-loader'},
       {test: /\.css$/, loader: "style-loader!css-loader!postcss-loader"}
     ]
   },
