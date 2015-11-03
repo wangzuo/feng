173a65477bdf5299d44ca0d6370106dc5c766e4b
Wang Zuo <wzuoadjusted@gmail.com>
2015-11-03 17:14:19 +0800

    copy html to layout

--- site/layout.js
+++ site/layout.js
@@ -4,7 +4,6 @@ var Footer = require('./footer');
 var Nav = require('./nav');
 var Grid = require('../react/grid');
 var sitemap = require('./map');
-var Html = require('./html');
 var pages = require('../pages')(sitemap);
 
 module.exports = React.createClass({
@@ -34,7 +33,18 @@ module.exports = React.createClass({
     var page = pages.filter((p) => (p.path === path))[0];
 
     return (
-      <Html>
+      <html>
+        <head>
+          <meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+          <title>Feng UI</title>
+          <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" type="text/css"/>
+          <link rel="stylesheet" type="text/css" href="/vendors/prism.css"/>
+          <link rel="stylesheet" href="/feng.min.css" type="text/css"/>
+          <link rel="stylesheet" href="/example.css" type="text/css"/>
+        </head>
+        <body>
+
         <div className="app">
           <Header/>
 
@@ -53,7 +63,13 @@ module.exports = React.createClass({
           </Grid.Container>
           <Footer/>
         </div>
-      </Html>
+
+        <script src="https://fb.me/react-0.13.3.js"></script>
+        <script src="/feng-ui/vendors/prism.js" data-manual></script>
+        <script src="/feng-ui/vendors/babel-browser.min.js"></script>
+        <script src="/feng-ui/app.js"></script>
+        </body>
+      </html>
     );
   }
 });
