9f7e9c2a0ec51bdb1deaea7ebfce9aeafe23c13b
Wang Zuo <wzuoadjusted@gmail.com>
2015-11-06 16:05:49 +0800

    remove feng-form

--- index.js
+++ index.js
@@ -7,18 +7,13 @@ module.exports = {
   Checkbox: require('./lib/checkbox'),
   Dialog: require('./lib/dialog'),
   Dropdown: require('./lib/dropdown'),
-  FengForm: require('./lib/feng-form'),
   Field: require('./lib/field'),
   Form: require('./lib/form'),
   Gravatar: require('./lib/gravatar'),
   Grid: require('./lib/grid'),
-  InputColor: require('./lib/input-color'),
-  InputSlider: require('./lib/input-slider'),
-  InputSwitch: require('./lib/input-switch'),
   Label: require('./lib/label'),
   Markdown: require('./lib/markdown'),
   Slat: require('./lib/slat'),
   Spinner: require('./lib/spinner'),
-  Tabs: require('./lib/tabs'),
-  Textarea: require('./lib/textarea')
+  Tabs: require('./lib/tabs')
 };
